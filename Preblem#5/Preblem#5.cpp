78e88f701d9f8494edbe5aef7a1a54fe8c20f98f
AHMEDZAINU <90534879+AHMEDZAINU@users.noreply.github.com>
2024-08-22 00:36:16 +0300

    Update Preblem#5.cpp

---
+++ Preblem#5/Preblem#5.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+float ReadPositiveNumper(string Message)
+{
+	float Number = 0;
+	do
+	{
+		cout << Message << endl;
+		cin >> Number;
+	} while (Number <= 0);
+
+	return Number;
+}
+
+void printerDigits(int Number)
+{
+  int Remainder = 0;
+  while (Number > 0)
+  {
+     Remainder =Number % 10;
+     Number = Number
+     cout << Remainder << endl;
+  }
+}
+int main()
+{
+  printerDigits(ReadPositiveNumper("please enter apositive number?"));
+  return 0;
+}
